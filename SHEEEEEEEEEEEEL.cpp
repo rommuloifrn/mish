2ef4a080c23f0cc878267f611a60117d6efcbb4c
rommuloifrn <roomulo.cavalkant@outlook.com>
2024-08-10 17:58:13 -0300

    feat: required commands structure

--- SHEEEEEEEEEEEEL.cpp
+++ SHEEEEEEEEEEEEL.cpp
@@ -24,51 +24,76 @@ vector<string> mish_args(string input) {
     return vec;
 }
 
+void execute(string path, string command) {
+    pid_t pid = fork();
+    if (pid < 0){ // Erro
+        std::cout << "Erro de execução!" << std::endl;
+        return;
+    } else if (pid == 0){ //processo filho
+        char * argv[2] = {(char *)command.c_str(), nullptr};
+        execve(path.c_str(), argv, NULL);
+    } else { // Processo pai
+        /* Deve adicionar processo filho na lista (std::vector)
+        de processos em execução para gerenciar background. */
+        /* Processo pai espera processo filho terminar. */
+        waitpid(pid, nullptr, 0);
+    }
+}
+
+void try_executing(string command, vector<string>& args) {
+    if ( (!args.empty()) && (args.back() == "&") ) // trata background
+            cout << "background não tratado!\n";
+
+
+    std::string absolute_path = "/bin/" + command;
+
+    if (access(absolute_path.c_str(), F_OK) == 0) { // Arquivo existe no diretório
+        if (access(absolute_path.c_str(), X_OK) == 0) { // Arquivo é executável
+            execute(absolute_path, command);
+        } else { // Arquivo não é executável
+            std::cout << "permission denied1: " << command << std::endl;
+        }
+    } else if (access(command.c_str(), F_OK) == 0) {
+        if (access(command.c_str(), X_OK) == 0) { // Arquivo é executável
+            execute("", command);
+        } else { // Arquivo não é executável
+            std::cout << "permission denied: " << command << std::endl;
+        }
+    }
+    else { // Arquivo não existe
+        std::cout << "Command not found: " << command << std::endl;
+    }
+}
+
 void process_command(std::string input) {
 
     string command = mish_command(input);
     vector<string> args = mish_args(input);
     
-    
-    // tratamento de comando em background
-    if (args.back() == "&")
-        cout << "ximbas!";
-
     // Se for comando interno...
     if (command == "exit")
         exit(0);
+    else if (command == "pwd")
+        cout << "pwd não implementado!\n";
+    else if (command == "cd")
+        cout << "cd não implementado!\n";
+    else if (command == "history")
+        cout << "history não implementado!\n";
+    else {
+        // Se for comando externo
 
+        // * necessário verificar se é para ser executado em background
+        /*  Se for caminho relativo, procurar o comando na lista de diretórios
+            Se for absoluto verifica se comando existe
+        */
+
+        // tratamento de comando em background
+        try_executing(command, args);
+        
+    }
     
 
-    // Se for comando externo
-
-    // * necessário verificar se é para ser executado em background
-    /*  Se for caminho relativo, procurar o comando na lista de diretórios
-        Se for absoluto verifica se comando existe
-    */
-    std::string absolute_path = "/bin/" + command;
-
-    if (access(absolute_path.c_str(), F_OK) == 0) { // Arquivo existe no diretório
-        if (access(absolute_path.c_str(), X_OK) == 0) { // Arquivo é executável
-            pid_t pid = fork();
-            if (pid < 0){ // Erro
-                std::cout << "Erro de execução!" << std::endl;
-                return;
-            } else if (pid == 0){ //processo filho
-                char * argv[2] = {(char *)command.c_str(), nullptr};
-                execve(absolute_path.c_str(), argv, NULL);
-            } else { // Processo pai
-                /* Deve adicionar processo filho na lista (std::vector)
-                   de processos em execução para gerenciar background. */
-                /* Processo pai espera processo filho terminar. */
-                waitpid(pid, nullptr, 0);
-            }
-        } else { // Arquivo não é executável
-            std::cout << "permission denied: " << command << std::endl;
-        }
-    } else { // Arquivo não existe
-        std::cout << "Command not found: " << command << std::endl;
-    }
+    
 }
 
 int main() {
